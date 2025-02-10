fa3a31a7e6b6992a855e5cfbbe300cdae717a1b9
Javid Izadi <javid.izadi1384@gmail.com>
2025-02-11 02:15:24 +0330

    fix: wrong color space

--- src/swap_chain.cpp
+++ src/swap_chain.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <limits>
 #include <stdexcept>
+#include <vulkan/vulkan_core.h>
 
 namespace lve {
 
@@ -364,7 +365,7 @@ void SwapChain::createSyncObjects() {
 VkSurfaceFormatKHR SwapChain::chooseSwapSurfaceFormat(
     const std::vector<VkSurfaceFormatKHR> &availableFormats) {
   for (const auto &availableFormat : availableFormats) {
-    if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM &&
+    if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
         availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
       return availableFormat;
     }
